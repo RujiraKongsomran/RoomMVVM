b12d0d4a34cb6ce7584a8726ebb2853b2b570508
Rujira Kongsomran <biwberry@outlook.com>
2020-01-05 23:17:27 +0700

    RecycleView + Adapter

--- app/src/main/java/com/rujira/roommvvm/MainActivity.java
+++ app/src/main/java/com/rujira/roommvvm/MainActivity.java
@@ -3,6 +3,8 @@ package com.rujira.roommvvm;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -19,12 +21,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter noteAdapter = new NoteAdapter();
+        recyclerView.setAdapter(noteAdapter);
+
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                // Update RecyclerView
-                Toast.makeText(MainActivity.this, "onChange", Toast.LENGTH_SHORT).show();
+                noteAdapter.setNotes(notes);
             }
         });
     }
