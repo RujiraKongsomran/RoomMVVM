c435ad9c8bfca997a3099fbf63144c3c9ec3902f
Rujira Kongsomran <biwberry@outlook.com>
2020-01-05 22:27:21 +0700

    Write code in Activity

--- app/src/main/java/com/rujira/roommvvm/MainActivity.java
+++ app/src/main/java/com/rujira/roommvvm/MainActivity.java
@@ -1,14 +1,31 @@
 package com.rujira.roommvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.rujira.roommvvm.Model.Note;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // Update RecyclerView
+                Toast.makeText(MainActivity.this, "onChange", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
